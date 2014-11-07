95cd8b3119673206c1f1d0de866ae80c22a80c1d
ed <ejbyne@gmail.com>
2014-11-07 12:36:33 +0000

    3rd attempt

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,20 +1,8 @@
-# def divisible_by_three?(number)
-#   number % 3 == 0
-# end
-
-# def divisible_by_five?(number)
-#   number % 5 == 0
-# end
-
-# def divisible_by_fifteen?(number)
-#   number % 15 == 0
-# end
-
 def divisible_by?(number, divisor)
   number % divisor == 0
 end
-
-def say_fizzbuzz(number)
+  
+def fizzbuzz_says(number)
   return "Fizzbuzz" if divisible_by?(number, 15)
   return "Fizz" if divisible_by?(number, 3)
   return "Buzz" if divisible_by?(number, 5)
